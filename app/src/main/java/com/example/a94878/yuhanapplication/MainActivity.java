9649220de997f45ebe396f59ee918a69861acb04
Yuhan Hou <yhou@ufl.edu>
2021-09-21 16:22:07 -0700

    3

--- app/src/main/java/com/example/a94878/yuhanapplication/MainActivity.java
+++ app/src/main/java/com/example/a94878/yuhanapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a94878.yuhanapplication;
 
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,24 +17,24 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
     MyRecyclerViewAdapter adapter;
-    ArrayList<String> user = new ArrayList<>();
+    ArrayList<String> user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new HTTPReqTask().execute();
-
         // set up the RecyclerView
-        RecyclerView recyclerView = findViewById(R.id.rvusers);
+        recyclerView = findViewById(R.id.rvusers);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new MyRecyclerViewAdapter(this, user);
-        recyclerView.setAdapter(adapter);
+        user = new ArrayList<>();
+
+        new HTTPReqTask().execute();
     }
 
-    private static class HTTPReqTask extends AsyncTask<Void, Void, Void> {
+    private class HTTPReqTask extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... params) {
             HttpURLConnection urlConnection = null;
@@ -45,9 +46,14 @@ public class MainActivity extends AppCompatActivity {
 
                 BufferedReader rd = new BufferedReader(new InputStreamReader(in));
                 String line;
-                while ((line = rd.readLine()) != null) {
+                int count=0;
+                while ((line = rd.readLine()) != null && count<2000) {
                     Log.i("epic", line);
+                    user.add(0,line);
+                    count++;
                 }
+                adapter = new MyRecyclerViewAdapter(getApplicationContext(), user);
+                recyclerView.setAdapter(adapter);
             }
             catch (Exception e) {
                 e.printStackTrace();
