c2e9903ae6cf22bd99f028f1b2640477ed513906
Yuhan Hou <yhou@ufl.edu>
2021-09-21 17:30:34 -0700

    4

--- app/src/main/java/com/example/a94878/yuhanapplication/MainActivity.java
+++ app/src/main/java/com/example/a94878/yuhanapplication/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.a94878.yuhanapplication;
 
-import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
